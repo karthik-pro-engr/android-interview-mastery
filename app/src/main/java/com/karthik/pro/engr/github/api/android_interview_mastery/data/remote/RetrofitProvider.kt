7c89f46e16cc89029b0fc0feccb0f1cb1b95904b
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-09-24 18:37:39 +0530

    feat(serializer): implement converter for retrofit

--- app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/RetrofitProvider.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/RetrofitProvider.kt
@@ -1,14 +1,22 @@
 package com.karthik.pro.engr.github.api.android_interview_mastery.data.remote
 
 import com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.api.GithubApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 object RetrofitProvider {
     private const val BASE_URL = "https://api.github.com/"
-
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addConverterFactory(
+                json.asConverterFactory("application/json".toMediaType())
+            )
             .build()
     }
 
--- app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/dto/response/GitHubRepoDto.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/dto/response/GitHubRepoDto.kt
@@ -1,12 +1,18 @@
 package com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.dto.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GitHubRepoDto(
     val id: Long,
     val name: String,
     val description: String?,
     val language: String?,
-    val stargazers_count: Int,
-    val forks_count: Int,
+    @SerialName("stargazers_count")
+    val stargazersCount: Int,
+    @SerialName("forks_count")
+    val forksCount: Int,
     val topics:List<String>,
     val owner: GitHubOwnerDto
 )
