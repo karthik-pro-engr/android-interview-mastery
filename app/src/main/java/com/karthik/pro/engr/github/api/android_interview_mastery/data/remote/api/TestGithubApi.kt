325cd5c1b9d9d677fa2e8c0990d8f8f6ecb8b779
karthik-pro-engr <101930095+karthik-pro-engr@users.noreply.github.com>
2026-09-24 12:30:14 +0530

    feat(test-annotation): add annotation test

--- app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/api/TestGithubApi.kt
+++ app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/api/TestGithubApi.kt
@@ -1,5 +1,6 @@
 package com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.api
 
+import android.R
 import com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.dto.request.CreateIssueRequest
 import com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.dto.request.ReplaceIssueRequest
 import com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.dto.request.UpdateIssueRequest
@@ -9,55 +10,89 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
-interface GithubApi {
+interface TestGithubApi {
+    /*
+    1. Get repositories
+    GET /users/{username}/repos
+      sort and order are query parameters
+     */
 
     @GET("users/{username}/repos")
-    suspend fun repos(
-        @Path("username") username: String,
+    suspend fun getRepositories(
+        @Path("username") userName: String,
         @Query("sort") sort: String,
-        @Query("order") order: String,
-        @Header("Authorization") authorization: String
+        @Query("order") order: String
     ): List<GitHubRepoDto>
 
-    // POST /repos/{owner}/{repo}/issues
+    /**
+     * 2. Create issue
+     * POST /repos/{owner}/{repo}/issues
+     * {
+     *   "title": "...",
+     *   "body": "..."
+     * }
+     */
+
     @POST("repos/{owner}/{repo}/issues")
-    suspend fun createIssue(
+    suspend fun createIssues(
         @Path("owner") ownerName: String,
         @Path("repo") repoName: String,
         @Body issue: CreateIssueRequest
     ): Response<IssueDto>
 
-    //PUT /repos/{owner}/{repo}/issues/{issue_number}
+    /*
+    * 3. Replace issue
+    * PUT /repos/{owner}/{repo}/issues/{issue_number}
+    * */
 
     @PUT("repos/{owner}/{repo}/issues/{issue_number}")
+    suspend fun replaceIssue(
+        @Path("owner") ownerName: String,
+        @Path("repo") repoName: String,
+        @Path("issue_number") issueNumber: String,
+        @Body issue: ReplaceIssueRequest
+    ): Response<IssueDto>
+
+    /*
+    * 4. Partially update issue
+    * PATCH /repos/{owner}/{repo}/issues/{issue_number}
+    * */
+
+    @PATCH("repos/{owner}/{repo}/issues/{issue_number}")
     suspend fun updateIssue(
         @Path("owner") ownerName: String,
         @Path("repo") repoName: String,
-        @Path("issue_number") number: String,
-        @Body updateIssue: ReplaceIssueRequest
-    ): Response<IssueDto>
-
-    // @PATCH "repos/{owner}/{repo}/issues/{issue_number}"
-    @PATCH("repos/{owner}/{repo}/issues/{issue_number}")
-    suspend fun patchIssue(
-        @Path("owner") ownerName:String,
-        @Path("repo") repoName: String,
         @Path("issue_number") issueNumber: String,
-        @Body updateIssue:UpdateIssueRequest
+        @Body issue: UpdateIssueRequest
     ): Response<IssueDto>
 
-    // DELETE /repos/{owner}/{repo}/issues/{issue_number}
+    /*
+    * 5. Delete issue
+    * DELETE /repos/{owner}/{repo}/issues/{issue_number}
+    * */
+
     @DELETE("repos/{owner}/{repo}/issues/{issue_number}")
     suspend fun deleteIssue(
-        @Path("owner") ownerName:String,
+        @Path("owner") ownerName: String,
         @Path("repo") repoName: String,
         @Path("issue_number") issueNumber: String
     ): Response<Unit>
+
+    /*
+    * 6. Dynamic endpoint
+    * The server gives you:
+    * https://api.github.com/repositories?page=2
+    *  */
+    @GET
+    suspend fun dynamicUrl(
+        @Url url: String
+    ): Response<Unit>
+
 }
---
+++ app/src/main/java/com/karthik/pro/engr/github/api/android_interview_mastery/data/remote/dto/request/ReplaceIssueRequest.kt
@@ -0,0 +1,7 @@
+package com.karthik.pro.engr.github.api.android_interview_mastery.data.remote.dto.request
+
+data class ReplaceIssueRequest(
+    val title: String? = null,
+    val body: String? = null,
+    val state: String? = null
+)
